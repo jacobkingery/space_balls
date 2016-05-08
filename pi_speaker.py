ab3096c5dd2e7881fc3328ee50e18a56106dae0d
jagreene <austin.greene@students.olin.edu>
2016-05-08 19:58:27 +0000

    added random options for lose sound

--- pi_speaker.py
+++ pi_speaker.py
@@ -2,6 +2,7 @@ import usb.core
 import time
 import RPi.GPIO as GPIO
 import pygame
+import random
 
 class Speaker(object):
     def __init__(self):
@@ -11,10 +12,15 @@ class Speaker(object):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.IN)
         self.sounds = {
-            0: pygame.mixer.Sound('/home/pi/space_balls/sounds/start.ogg'),
-            1: pygame.mixer.Sound('/home/pi/space_balls/sounds/shoot.ogg'),
-            2: pygame.mixer.Sound('/home/pi/space_balls/sounds/hit.ogg'),
-            3: pygame.mixer.Sound('/home/pi/space_balls/sounds/lose.ogg')
+            0: [pygame.mixer.Sound('/home/pi/space_balls/sounds/start.ogg')],
+            1: [pygame.mixer.Sound('/home/pi/space_balls/sounds/shoot.ogg')],
+            2: [pygame.mixer.Sound('/home/pi/space_balls/sounds/hit.ogg')],
+            3: [pygame.mixer.Sound('/home/pi/space_balls/sounds/lose_evil.ogg'),
+                pygame.mixer.Sound('/home/pi/space_balls/sounds/lose_cmon.ogg'),
+                pygame.mixer.Sound('/home/pi/space_balls/sounds/lose_blimp.ogg'),
+                pygame.mixer.Sound('/home/pi/space_balls/sounds/lose_friends.ogg'),
+                pygame.mixer.Sound('/home/pi/space_balls/sounds/lose_goof.ogg'),
+                pygame.mixer.Sound('/home/pi/space_balls/sounds/lose_gunner.ogg')]
         }
         self.SEND_SOUND = 0;
         self.dev = usb.core.find(idVendor = 0x6666, idProduct = 0x0003)
@@ -29,7 +35,7 @@ class Speaker(object):
         sound = self.dev.ctrl_transfer(0xC0, self.SEND_SOUND, 0, 0, 1)[0]
         if sound in self.sounds:
             print 'Playing:', sound
-            self.sounds[sound].play()
+            random.choice(self.sounds[sound]).play()
         else:
             print 'Sound not found'
 
